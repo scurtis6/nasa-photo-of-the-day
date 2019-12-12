a71eff38955b14034b0ebd01b6ead7a358fe0752
Sierra Curtis <sierracurtis91@gmail.com>
2019-12-12 14:06:04 -0500

    install dependencies (sytled-components, bootstrap, react)

--- src/Components/NasaCard.js
+++ src/Components/NasaCard.js
@@ -3,14 +3,22 @@ import React from "react";
 const NasaCard = props => {
     return(
         <div className="nasa_card">
+        <h1>Astronomy Picture of the Day</h1>
+        <div className="top_content">
             <h2>Title: {props.title}</h2>
             <p>Date: {props.date}</p>
-            <img className="image" src={props.img} alt="universe"></img>
-            <p>{props.imgUrl}</p>
-            <p>{props.explanation}</p>
-            
+            <p>Select a new date: <input type='date' id='photoDate' min='1995-06-16' max='2019-12-11'></input></p>
         </div>
-        
+        <div className="image_content">
+            <a href={props.imgUrl} target="_blank" rel="noopener noreferrer">
+                <img onClick={() => props.imgUrl} className="image" title="Click to enlarge image" src={props.img} alt="universe"></img>  
+            </a>
+        </div>
+        <div className="explanation">
+            <h3>Image Explanation</h3>
+            <p>{props.explanation}</p>
+        </div>        
+    </div>  
     )
 }
 
